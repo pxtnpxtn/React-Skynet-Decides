0e6ff835f9db3c48cbb3387710f2800a4bbfac63
Paxton Eicher <paxton.w.eicher@gmail.com>
2017-11-23 16:57:41 -0500

    Skynet Decides to-do list app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SkynetApp from './components/SkynetApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<SkynetApp />, document.getElementById('App'));
